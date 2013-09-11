9bfc00dff0f8c654391a0ce31ee4244a89ad7c11
raman <raman@fbk.eu>
2013-09-11 11:26:23 +0200

    OAuth-based template with token-based authentication

--- src/main/java/smartcampus/webtemplate/controllers/ExampleController.java
+++ src/main/java/smartcampus/webtemplate/controllers/ExampleController.java
@@ -1,3 +1,18 @@
+/**
+ *    Copyright 2012-2013 Trento RISE
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
 package smartcampus.webtemplate.controllers;
 
 import it.sayservice.platform.smartplanner.data.message.Itinerary;
@@ -7,106 +22,77 @@ import it.sayservice.platform.smartplanner.data.message.TType;
 import it.sayservice.platform.smartplanner.data.message.journey.SingleJourney;
 import it.sayservice.platform.smartplanner.data.message.otpbeans.Route;
 
-import java.io.IOException;
 import java.util.Collections;
 import java.util.List;
-import java.util.ArrayList;
-import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
-import org.apache.log4j.Logger;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import eu.trentorise.smartcampus.ac.provider.AcService;
-import eu.trentorise.smartcampus.ac.provider.filters.AcProviderFilter;
 import eu.trentorise.smartcampus.communicator.CommunicatorConnector;
 import eu.trentorise.smartcampus.communicator.CommunicatorConnectorException;
 import eu.trentorise.smartcampus.communicator.model.Notification;
-import eu.trentorise.smartcampus.discovertrento.DiscoverTrentoConnector;
-import eu.trentorise.smartcampus.dt.model.EventObject;
-import eu.trentorise.smartcampus.dt.model.ObjectFilter;
 import eu.trentorise.smartcampus.filestorage.client.Filestorage;
 import eu.trentorise.smartcampus.filestorage.client.FilestorageException;
-import eu.trentorise.smartcampus.filestorage.client.model.AppAccount;
+import eu.trentorise.smartcampus.filestorage.client.model.Account;
 import eu.trentorise.smartcampus.filestorage.client.model.Metadata;
-import eu.trentorise.smartcampus.filestorage.client.model.UserAccount;
-import eu.trentorise.smartcampus.journeyplanner.JourneyPlannerConnector;
-import eu.trentorise.smartcampus.profileservice.ProfileConnector;
+import eu.trentorise.smartcampus.mobilityservice.MobilityDataService;
+import eu.trentorise.smartcampus.mobilityservice.MobilityPlannerService;
+import eu.trentorise.smartcampus.mobilityservice.MobilityServiceException;
+import eu.trentorise.smartcampus.profileservice.BasicProfileService;
+import eu.trentorise.smartcampus.profileservice.ProfileServiceException;
 import eu.trentorise.smartcampus.profileservice.model.BasicProfile;
+import eu.trentorise.smartcampus.social.model.Group;
 import eu.trentorise.smartcampus.socialservice.SocialService;
 import eu.trentorise.smartcampus.socialservice.SocialServiceException;
-import eu.trentorise.smartcampus.socialservice.model.Group;
+import eu.trentorise.smartcampus.territoryservice.TerritoryService;
+import eu.trentorise.smartcampus.territoryservice.TerritoryServiceException;
+import eu.trentorise.smartcampus.territoryservice.model.EventObject;
+import eu.trentorise.smartcampus.territoryservice.model.ObjectFilter;
 
-@Controller("exampleController")
+@Controller
 public class ExampleController {
 
-	private static final String EVENT_OBJECT = "eu.trentorise.smartcampus.dt.model.EventObject";
-	private static final Logger logger = Logger
-			.getLogger(ExampleController.class);
-	@Autowired
-	private AcService acService;
+	private BasicProfileService profileService = new BasicProfileService("https://vas-dev.smartcampuslab.it/aac");
+	private MobilityDataService mobilityDataService = new MobilityDataService("https://vas-dev.smartcampuslab.it/core.mobility");
+	private MobilityPlannerService mobilityPlannerService = new MobilityPlannerService("https://vas-dev.smartcampuslab.it/core.mobility");
+	private TerritoryService territoryService = new TerritoryService("https://vas-dev.smartcampuslab.it/core.territory");
+	private CommunicatorConnector communicatorConnector = null;
+	private Filestorage filestorage = new Filestorage("https://vas-dev.smartcampuslab.it/core.filestorage", "rk");
+	private SocialService socialService = new SocialService("https://vas-dev.smartcampuslab.it/core.social");
 
-	/*
-	 * the base url of the service. Configure it in webtemplate.properties
-	 */
-	@Autowired
-	@Value("${services.server}")
-	private String serverAddress;
-
-	/*
-	 * the base appName of the service. Configure it in webtemplate.properties
-	 */
-	@Autowired
-	@Value("${webapp.name}")
-	private String appName;
+	public ExampleController() {
+		super();
+		try {
+			communicatorConnector = new CommunicatorConnector("https://vas-dev.smartcampuslab.it/core.communicator", "rk");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 
 	/*
 	 * Example to get the profile of the authenticated user.
 	 */
-	@RequestMapping(method = RequestMethod.GET, value = "/getprofile")
+	@RequestMapping(method = RequestMethod.GET, value = "/profile")
 	public @ResponseBody
-	BasicProfile getProfile(HttpServletRequest request,
-			HttpServletResponse response, HttpSession session)
-			throws IOException {
-		try {
-			String token = request.getHeader(AcProviderFilter.TOKEN_HEADER);
-			ProfileConnector profileConnector = new ProfileConnector(
-					serverAddress);
-			BasicProfile profile = profileConnector.getBasicProfile(token);
-			return profile;
-		} catch (Exception e) {
-			response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
-		}
-		return null;
+	BasicProfile getProfile(HttpServletRequest request) throws SecurityException, ProfileServiceException {
+		BasicProfile profile = profileService.getBasicProfile(getToken(request));
+		return profile;
 	}
 
 	/*
 	 * Request all the routes for Trentino Trasporti (agencyId = "12")
 	 */
-	@RequestMapping(method = RequestMethod.GET, value = "/getroutes")
+	@RequestMapping(method = RequestMethod.GET, value = "/routes")
 	public @ResponseBody
-	List<Route> getRoutes(HttpServletRequest request,
-			HttpServletResponse response, HttpSession session)
-			throws IOException {
-		try {
-			String token = request.getHeader(AcProviderFilter.TOKEN_HEADER);
-			JourneyPlannerConnector journeyPlannerConnector = new JourneyPlannerConnector(
-					serverAddress);
-			List<Route> routes = journeyPlannerConnector.getRoutes("12", token);
-			return routes;
-		} catch (Exception e) {
-			response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
-		}
-		return null;
+	List<Route> getRoutes(HttpServletRequest request)
+			throws MobilityServiceException {
+		List<Route> routes = mobilityDataService.getRoutes("12", getToken(request));
+		return routes;
 	}
 
 	/*
@@ -114,130 +100,69 @@ public class ExampleController {
 	 */
 	@RequestMapping(method = RequestMethod.GET, value = "/plansinglejourney")
 	public @ResponseBody
-	List<Itinerary> planSingleJourney(HttpServletRequest request,
-			HttpServletResponse response, HttpSession session)
-			throws IOException {
-		try {
-			String token = request.getHeader(AcProviderFilter.TOKEN_HEADER);
-			JourneyPlannerConnector journeyPlannerConnector = new JourneyPlannerConnector(
-					serverAddress);
-			SingleJourney req = new SingleJourney();
-			req.setDate("03/28/2013");
-			req.setDepartureTime("10:25");
-			Position from = new Position();
-			from.setLat("46.062005");
-			from.setLon("11.129169");
-			Position to = new Position();
-			to.setLat("46.068854");
-			to.setLon("11.151184");
-			req.setFrom(from);
-			req.setTo(to);
-			TType[] tt = new TType[] { TType.TRANSIT };
-			req.setTransportTypes(tt);
-			req.setResultsNumber(1);
-			req.setRouteType(RType.fastest);
+	List<Itinerary> planSingleJourney(HttpServletRequest request) throws MobilityServiceException {
+		SingleJourney req = new SingleJourney();
+		req.setDate("03/28/2013");
+		req.setDepartureTime("10:25");
+		Position from = new Position();
+		from.setLat("46.062005");
+		from.setLon("11.129169");
+		Position to = new Position();
+		to.setLat("46.068854");
+		to.setLon("11.151184");
+		req.setFrom(from);
+		req.setTo(to);
+		TType[] tt = new TType[] { TType.TRANSIT };
+		req.setTransportTypes(tt);
+		req.setResultsNumber(1);
+		req.setRouteType(RType.fastest);
 
-			List<Itinerary> itineraries = journeyPlannerConnector
-					.planSingleJourney(req, token);
-			return itineraries;
-		} catch (Exception e) {
-			response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
-		}
-		return null;
+		List<Itinerary> itineraries = mobilityPlannerService.planSingleJourney(req, getToken(request));
+		return itineraries;
 	}
 
 	/*
 	 * Get all the events whose category is "Concerts"
 	 */
-	@RequestMapping(method = RequestMethod.GET, value = "/getconcerts")
+	@RequestMapping(method = RequestMethod.GET, value = "/concerts")
 	public @ResponseBody
-	List<EventObject> getConterts(HttpServletRequest request,
-			HttpServletResponse response, HttpSession session)
-			throws IOException {
-		try {
-			String token = request.getHeader(AcProviderFilter.TOKEN_HEADER);
-			DiscoverTrentoConnector discoverTrentoConnector = new DiscoverTrentoConnector(
-					serverAddress);
-			ObjectFilter filter = new ObjectFilter();
-		//	filter.setClassName(EVENT_OBJECT);
-		//	filter.setTypes(Collections.singletonList("Concerts"));
-			filter.setFromTime(System.currentTimeMillis());
-			Map<String, List<?>> result = discoverTrentoConnector.getObjects(
-					filter, token);
-
-			@SuppressWarnings("unchecked")
-			List<EventObject> list = (List<EventObject>) result.get(EVENT_OBJECT);
-			return list;
-		} catch (Exception e) {
-			response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
-		}
-		return null;
+	List<EventObject> getConterts(HttpServletRequest request) throws TerritoryServiceException {
+		ObjectFilter filter = new ObjectFilter();
+		filter.setTypes(Collections.singletonList("Concerts"));
+		filter.setFromTime(System.currentTimeMillis());
+		return territoryService.getEvents(filter, getToken(request));
 	}
 
 	/*
 	 * Get all notifications
 	 */
-	@RequestMapping(method = RequestMethod.GET, value = "/getnotifications")
+	@RequestMapping(method = RequestMethod.GET, value = "/notification")
 	public @ResponseBody
-	List<Notification> getNotifications(HttpServletRequest request,
-			HttpServletResponse response, HttpSession session)
-			throws IOException {
-		try {
-			String token = request.getHeader(AcProviderFilter.TOKEN_HEADER);
-
-			CommunicatorConnector communicatorConnector = new CommunicatorConnector(
-					serverAddress);
-
-			List<Notification> result = communicatorConnector.getNotifications(
-					0L, 0, -1, token);
-
-			return (List<Notification>) result;
-		} catch (Exception e) {
-			response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
-		}
-		return null;
-	}
-
-	/*
-	 * Example to get all storage application accounts binded to a specific
-	 * application. Example uses tutorial application account named 'hackathon'.
-	 */
-	@RequestMapping(method = RequestMethod.GET, value = "/appaccount")
-	public @ResponseBody
-	List<AppAccount> getAppAccounts(HttpServletRequest request)
-			throws FilestorageException {
-		Filestorage filestorage = new Filestorage(serverAddress, "hackathon");
-		return filestorage.getAppAccounts();
+	List<Notification> getNotifications(HttpServletRequest request) throws CommunicatorConnectorException {
+		List<Notification> result = communicatorConnector.getNotificationsByUser(0L, 0, -1, getToken(request)).getNotifications();
+		return (List<Notification>) result;
 	}
 
 	/*
 	 * Example to get all storage user accounts owned by the authenticated user
-	 * binded to an application . Example uses tutorial application account
-	 * named 'hackathon'
+	 * bound to an application.
 	 */
 	@RequestMapping(method = RequestMethod.GET, value = "/useraccount")
 	public @ResponseBody
-	List<UserAccount> getuserAccounts(HttpServletRequest request)
-			throws FilestorageException {
-		String token = request.getHeader(AcProviderFilter.TOKEN_HEADER);
-		Filestorage filestorage = new Filestorage(serverAddress, "hackathon");
-
-		return filestorage.getUserAccounts(token);
+	Account getUserAccounts(HttpServletRequest request) throws FilestorageException {
+		return filestorage.getAccountByUser(getToken(request));
 	}
 
 	/*
-	 * Example to get the information about a stored resource. Example uses the
-	 * tutorial application account named 'hackathon' and a default resource
-	 * already stored (resourceId = 513da746975aa4412a383769
+	 * Example to get the information about a stored resource. 
 	 */
 	@RequestMapping(method = RequestMethod.GET, value = "/metadata")
 	public @ResponseBody
 	Metadata getResourceMetadata(HttpServletRequest request)
 			throws FilestorageException {
-		String token = request.getHeader(AcProviderFilter.TOKEN_HEADER);
-		Filestorage filestorage = new Filestorage(serverAddress, "hackathon");
-		return filestorage.getResourceMetadata(token,
-				"513da746975aa4412a383769");
+		List<Metadata> list = filestorage.getAllResourceMetadataByApp(getToken(request), 0, 1);
+		if (list != null && ! list.isEmpty()) return list.get(0);
+		return null;
 	}
 
 
@@ -248,14 +173,17 @@ public class ExampleController {
 	 */
 	@RequestMapping(method = RequestMethod.GET, value = "/group")
 	public @ResponseBody
-	List<Group> getUsergroups(HttpServletRequest request)
-			throws SecurityException, SocialServiceException {
-		String token = request.getHeader(AcProviderFilter.TOKEN_HEADER);
-		SocialService socialsrv = new SocialService(serverAddress);
-		return socialsrv.getGroups(token);
+	List<Group> getUsergroups(HttpServletRequest request) throws SecurityException, SocialServiceException {
+		return socialService.getUserGroups(getToken(request)).getContent();
 	}
 
-	
+	/**
+	 * Here we assume that the access token is placed in the current security context by the PRE_AUTH_FILTER
+	 * @return
+	 */
+	private String getToken(HttpServletRequest request) {
+		return (String)SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+	}
 
 
 }
